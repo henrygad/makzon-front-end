0b8cbee1bf449783f0719d466d237f8ee55c2ee8
Henry Loveday <henrygad.orji@gmail.com>
2025-07-18 08:42:16 +0100

    routes changes made on vite.config base property

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // ✅ THIS IS CRITICAL
   plugins: [react()],
   server: {
-    /*  proxy: {
-      "/api": {
-        target: "https://localhost:3000",
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, "")
-      },
-    }, */
     https: {
       key: "certs/localhost-key.pem",
       cert: "certs/localhost.pem",
     },
   },
 });
-
