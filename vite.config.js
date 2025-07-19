7de519018c827651216c27fb0d7cb6ef024a86be
Henry Loveday <henrygad.orji@gmail.com>
2025-07-19 11:11:22 +0100

    changes on blogcomment component

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // ✅ THIS IS CRITICAL
+ // base: "/", // ✅ THIS IS CRITICAL
   plugins: [react()],
   server: {
     https: {
